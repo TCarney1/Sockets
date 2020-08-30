f58c3a3be03cb735e2b3c98a4b514875049cbd8c
tomcarney <thomas.carney@griffithuni.edu.au>
2020-08-30 11:19:10 +1000

    sending files between server and client complete.

---
+++ server/server_header.h
@@ -0,0 +1,27 @@
+//
+// Created by Tom Carney on 28/8/20.
+//
+
+
+#ifndef SOCKETS_HEADER_H
+#define SOCKETS_HEADER_H
+
+
+#define PORT_NUM 80
+#define BUFF_SIZE 50
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+
+
+
+
+#endif //SOCKETS_HEADER_H
